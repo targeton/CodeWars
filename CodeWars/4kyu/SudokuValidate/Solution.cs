dc1c700dfb6b21a442cfd72dd8978edb7c28f43a
zhoupeng <target_on@126.com>
2019-01-30 22:22:15 +0800

    clever solution for sudoku

--- CodeWars/4kyu/SudokuValidate/Solution.cs
+++ CodeWars/4kyu/SudokuValidate/Solution.cs
@@ -1,55 +1,32 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CodeWars._4kyu.SudokuValidate
 {
     public class Sudoku
     {
-        bool _formatFlag = false;
         int[][] _data;
-        int _delta;
+
         public Sudoku(int[][] sudokuData)
         {
             //Constructor here
-            int len = sudokuData.Length;
-            int n = (int)Math.Sqrt(len);
-            if (len != n * n)
-                return;
-            _delta = n;
-            if (sudokuData.Any(s => s.Length != len))
-                return;
-            _formatFlag = true;
             _data = sudokuData;
         }
         //利用_rowCheck,_colCheck,_gridCheck记录每行、每列和每个片区内的是否有重复值，通过标志位来检测
         public bool IsValid()
         {
-            if (!_formatFlag)
-                return false;
-            int[] rowCheck = new int[_data.Length];
-            int[] colCheck = new int[_data.Length];
-            int[] gridCheck = new int[_data.Length];
-            for (int i = 0; i < _data.Length; i++)
-            {
-                for (int j = 0; j < _data[i].Length; j++)
-                {
-                    int n = _data[i][j];
-                    if ((rowCheck[i] & (1 << n)) != 0)
-                        return false;
-                    else
-                        rowCheck[i] |= (1 << n);
-                    if ((colCheck[j] & (1 << n)) != 0)
-                        return false;
-                    else
-                        colCheck[j] |= (1 << n);
-                    int pos = (i / _delta) * _delta + j / _delta;
-                    if ((gridCheck[pos] & (1 << n)) != 0)
-                        return false;
-                    else
-                        gridCheck[pos] |= (1 << n);
-                }
-            }
-            return true;
+            int n = _data.Length;
+            int m = (int)Math.Sqrt(n);
+            int e = m * m;
+            int sum = (e * e + e) / 2;
+            return Enumerable.Range(0, n)
+                .SelectMany(i => new[] 
+                { 
+                    _data[i].Sum(),
+                    _data.Sum(d => d[i]),
+                    _data.Skip(m*(i/m)).Take(m).SelectMany(r=>r.Skip(m*(i%m)).Take(m)).Sum()
+                }).All(i => i == sum);
         }
     }
 }
