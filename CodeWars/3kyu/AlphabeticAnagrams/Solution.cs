e139fd0ddcfff53cc00f12adbba63485b5018311
zhoupeng <target_on@126.com>
2019-02-16 20:59:42 +0800

    Add a new solution(3kyu AlphabeticAnagrams)

---
+++ CodeWars/3kyu/AlphabeticAnagrams/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars._3kyu.AlphabeticAnagrams
+{
+    public class Solution
+    {
+        public static long ListPosition(string value)
+        {
+            if (value.Length <= 1)
+                return 1;
+            long result = 0;
+            char[] temp = value.ToCharArray();
+            Dictionary<char, int> dic = temp.Distinct().ToDictionary(c => c, c => temp.Count(a => a == c));
+            char[] arr = temp.Distinct().OrderBy(t => t).ToArray();
+            int length = value.Length;
+            var more = temp.Where(t => t < temp[0]).Distinct();
+            if (more != null && more.Count() > 0)
+            {
+                foreach (var item in more)
+                {
+                    var tt = fact(length - 1);
+                    foreach (var key in dic.Keys)
+                    {
+                        if (key == item)
+                            tt = tt / fact(dic[key] - 1);
+                        else
+                            tt = tt / fact(dic[key]);
+                    }
+                    result += tt;
+                }
+            }
+
+            result += ListPosition(value.Substring(1, value.Length - 1));
+            return result;
+        }
+
+        public static long fact(long n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return n * fact(n - 1);
+        }
+    }
+}
---
+++ CodeWarsTest/3kyu/AlphabeticAnagrams/SolutionTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsTest._3kyu.AlphabeticAnagrams
+{
+    using System;
+    using NUnit.Framework;
+    using CodeWars._3kyu.AlphabeticAnagrams;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [TestCase("A", 1)]
+        [TestCase("ABAB", 2)]
+        [TestCase("AAAB", 1)]
+        [TestCase("BAAA", 4)]
+        [TestCase("QUESTION", 24572)]
+        [TestCase("BOOKKEEPER", 10743)]
+        public void TestNumberToOrdinal(string value, long expected)
+        {
+            Assert.AreEqual(expected, Solution.ListPosition(value), string.Format("Input {0}", value));
+        }
+    }
+}
